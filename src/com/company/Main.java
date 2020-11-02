3e9fd36039ed0f51d0184678952a1cddcee0bcc6
SofiaAlves <alveslaura.sofia@gmail.com>
2020-11-02 14:51:27 +0100

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // menu qui propose à l'utlilisateur de choisir l'exercice qui voudra lancer
+        // parmi les options du menu il y aura l'option quitter
+
+
+
+
+    }
+}
